cdd0f44bd4402096f1031407b370728144047665
elhajuojy <souirimehdi311@gmail.com>
2023-03-11 15:24:10 +0100

    first steup for the plugin

---
+++ wp-content/plugins/feedback-test/feedback-test.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: Feedback Test
+ * Plugin URI: http://www.example.com
+ * Description: This is a test plugin for feedback
+ * Version: 1.0
+ * Author: ELMahdi elhjuojy
+ * Author URI: http://www.example.com
+ * License: GPL2
+*/
+
